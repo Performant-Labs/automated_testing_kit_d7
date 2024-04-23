55271571b6c715386002dbffbc17302cbe49c282
Ilya Lyaukin <kzerby@gmail.com>
2024-04-23 12:24:44 -0600

    ATK-PW-1000 Update tests for login, registration and atk_commands for Drupal 7. Use different emails for different tests.

--- playwright/e2e/atk_contact_us/atk_contact_us.spec.js
+++ playwright/e2e/atk_contact_us/atk_contact_us.spec.js
@@ -10,14 +10,6 @@
 
 import * as atkCommands from '../support/atk_commands';
 import * as atkUtilities from '../support/atk_utilities';
-
-// Set up Playwright.
-const { test, expect } = require('@playwright/test');
-
-import playwrightConfig from '../../playwright.config';
-
-const baseUrl = playwrightConfig.use.baseURL;
-
 // Import ATK Configuration.
 import atkConfig from '../../playwright.atk.config';
 
@@ -27,7 +19,10 @@ import userEtherealAccount from '../data/etherealUser.json';
 // Standard accounts that use user accounts created
 // by QA Accounts. QA Accounts are created when the QA
 // Accounts module is enabled.
-import qaUserAccounts from '../data/qaUsers.json';
+import qaUsers from '../data/qaUsers.json';
+
+// Set up Playwright.
+const { test, expect } = require('@playwright/test');
 
 test.describe('Contact Us tests.', () => {
   //
@@ -37,7 +32,6 @@ test.describe('Contact Us tests.', () => {
     const uniqueToken = atkUtilities.createRandomString(6);
     const testId = '(ATK-PW-1050)';
     const subjectLine = `${testId} ${uniqueToken}`;
-    let textContent = '';
 
     // Begin registration.
     await page.goto(atkConfig.contactUsUrl);
@@ -47,30 +41,9 @@ test.describe('Contact Us tests.', () => {
     await page.getByLabel('Subject').fill(subjectLine);
     await page.getByLabel('Message').fill(testId);
     await page.getByRole('button', { name: 'Send message' }).click();
-
-    // The status box needs a moment to appear.
-    const message = await page.waitForSelector('.messages.status');
-
-    // Should see the thank-you message.
-    // textContent = await page.content();
-    expect(await message.textContent()).toContain('Your message has been sent.');
+    await atkCommands.expectMessage(page, 'Your message has been sent.');
 
     // Check for Contact Us email at Ethereal.
-    const etherealUrl = 'https://ethereal.email';
-    await page.goto(`${etherealUrl}/login`);
-    await page.getByPlaceholder('Enter email').fill(userEtherealAccount.userEmail);
-    await page.getByPlaceholder('Password').fill(userEtherealAccount.userPassword);
-    await page.getByRole('button', { name: 'Log in' }).click();
-
-    textContent = await page.textContent('body');
-    expect(textContent).toContain(`Logged in as ${userEtherealAccount.userEmail}`);
-
-    await page.goto(`${etherealUrl}/messages`);
-
-    textContent = await page.textContent('body');
-    expect(textContent).toContain(`Messages for ${userEtherealAccount.userEmail}`);
-
-    // Look for "(ATK-CY-1050) uniqueToken" generated above.
-    await expect(page.getByRole('row', { subject: subjectLine })).toBeVisible;
+    await atkCommands.checkEmail(page, null, subjectLine);
   });
 });
